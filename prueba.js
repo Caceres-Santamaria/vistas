fcb55954987eaff138691c52a0904ecb6dd76f6b
jmsr06 <jennysantamaria06@gmail.com>
2020-08-08 21:30:02 -0500

    pueba slider producto

---
+++ prueba.js
@@ -0,0 +1,31 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+	var secondarySlider = new Splide( '#secondary-slider', {
+		fixedWidth  : 80,
+		height      : 80,
+		gap         : 8,
+		cover       : true,
+		isNavigation: true,
+		focus       : 'center',
+		pagination : false,
+		arrows     : true,
+		breakpoints : {
+			'600': {
+				fixedWidth: 66,
+				height    : 40,
+			}
+		},
+	} ).mount();
+	
+	var primarySlider = new Splide( '#primary-slider', {
+		type       : 'fade',
+		heightRatio: 0.5,
+		pagination : false,
+		arrows     : false,
+		cover      : true,
+	} ); // do not call mount() here.
+	
+	primarySlider.sync( secondarySlider ).mount();
+} );
+
+
+
