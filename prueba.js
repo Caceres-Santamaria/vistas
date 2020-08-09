29c259bbab44e6a0ed16e12dd182b2bd36104e9a
Alexis3004 <nelsonalexiscacerzcarreo37@gmail.com>
2020-08-08 22:44:44 -0500

    slider de producto terminada

--- prueba.js
+++ prueba.js
@@ -1,31 +1,30 @@
-document.addEventListener( 'DOMContentLoaded', function () {
-	var secondarySlider = new Splide( '#secondary-slider', {
-		fixedWidth  : 80,
-		height      : 80,
-		gap         : 8,
-		cover       : true,
-		isNavigation: true,
-		focus       : 'center',
-		pagination : false,
-		arrows     : true,
-		breakpoints : {
-			'600': {
-				fixedWidth: 66,
-				height    : 40,
-			}
-		},
-	} ).mount();
-	
-	var primarySlider = new Splide( '#primary-slider', {
-		type       : 'fade',
-		heightRatio: 0.5,
-		pagination : false,
-		arrows     : false,
-		cover      : true,
-	} ); // do not call mount() here.
-	
-	primarySlider.sync( secondarySlider ).mount();
-} );
+document.addEventListener('DOMContentLoaded', function() {
+    var secondarySlider = new Splide('#secondary-slider', {
+        fixedWidth: 80,
+        height: 80,
+        gap: 8,
+        cover: true,
+        isNavigation: true,
+        focus: 'center',
+        pagination: false,
+        arrows: true,
+        breakpoints: {
+            '600': {
+                fixedWidth: 66,
+                height: 40,
+            }
+        },
+    }).mount();
 
+    var primarySlider = new Splide('#primary-slider', {
+        fixedWidth: 300,
+        height: 300,
+        type: 'fade',
+        heightRatio: 0.5,
+        pagination: false,
+        arrows: false,
+        cover: true,
+    }); // do not call mount() here.
 
-
+    primarySlider.sync(secondarySlider).mount();
+});
